9d2fe8b89ae933796a6b2269e95e5905c92e5617
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-01-25 11:41:31 +0300

    Config change

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     IS_DEVELOPMENT: process.env.NODE_ENV !== "production",
     DOMAIN: process.env.DOMAIN,
-    PORT: process.env.PORT | 3000,
+    PORT: process.env.PORT,
     API_URL: process.env.API_URL,
     DATABASE_URL: process.env.DATABASE_URL
 }
